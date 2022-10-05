c7498c0d7869f63474f1dc2a0c26ff72d736335b
Ferdinand Malcher <ferdinand@malcher.media>
2022-10-05 12:49:34 +0200

    Interface Book

--- book-rating/src/app/dashboard/dashboard.component.ts
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'br-dashboard',
@@ -6,10 +7,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  books: Book[] = [];
 
-  constructor() { }
+  constructor() {
+    this.books = [
+      // TODO
+    ];
+  }
 
   ngOnInit(): void {
   }
 
 }
+
+
+/*
+isbn
+authors
+title
+publisher
+pages
+price
+thumbnailUrl
+description
+subtitle
+
+*/
+/*
+class BookC implements BookI {
+  rating = 5;
+  constructor(public isbn: string, public title: string) {}
+
+  rateUp() {
+    this.rating++;
+  }
+}
+
+
+const myBook = new BookC('123', 'Angular');
+myBook.rateUp();
+
+//////////
+
+
+interface BookI {
+  isbn: string;
+  title: string;
+  rating: number;
+}
+
+const myBook2: BookI = {
+  isbn: '123',
+  title: 'Angular',
+  rating: 5,
+};
+
+function rateUp(book: BookI): BookI {
+  book.rating++;
+  return book;
+}*/
