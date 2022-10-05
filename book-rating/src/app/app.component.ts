86174968e0f923748954a48535157b54ed719d89
Ferdinand Malcher <ferdinand@malcher.media>
2022-10-05 12:02:07 +0200

    Dashboard generieren

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'br-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  // template: '<h1>FOO</h1>', // Inline Template
+  // styles: ['h1.foo { color: blue }'] // Inline Style
 })
 export class AppComponent {
-  title = 'book-rating';
+  title = 'Book Rating!';
+
+  constructor() {
+    setTimeout(() => {
+      this.title = 'Hallo Welt';
+    }, 2000);
+  }
 }
