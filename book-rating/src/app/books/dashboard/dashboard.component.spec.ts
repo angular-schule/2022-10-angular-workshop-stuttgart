56f57d4659d275e21c1eae97fed89add22f9e943
Ferdinand Malcher <ferdinand@malcher.media>
2022-10-07 11:20:03 +0200

    Test DashboardComponent

--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Book } from '../shared/book';
+import { BookRatingService } from '../shared/book-rating.service';
 
 import { DashboardComponent } from './dashboard.component';
 
@@ -7,8 +9,24 @@ describe('DashboardComponent', () => {
   let fixture: ComponentFixture<DashboardComponent>;
 
   beforeEach(async () => {
+
+    // Ersatz für den BookRatingService
+    const ratingMock: Partial<BookRatingService> = {
+      rateUp: (b: Book) => {
+        return b;
+      },
+      rateDown: (b: Book) => {
+        return b;
+      }
+    };
+
     await TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ DashboardComponent ],
+      providers: [
+        // BRS ersetzen: Immer wenn jemand BRS anfordert,
+        // wird stattdessen das Objekt ratingMock ausgeliefert
+        { provide: BookRatingService, useValue: ratingMock }
+      ]
     })
     .compileComponents();
 
@@ -24,5 +42,29 @@ describe('DashboardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call the service for doRateUp()', () => {
+    // BRS anfordern, in Wahrheit ist das aber der ratingMock
+    const service = TestBed.inject(BookRatingService);
+
+    // Mock überwachen
+    // spyOn(service, 'rateUp').and.returnValue({ isbn: '' } as Book);
+    // spyOn(service, 'rateUp').and.callFake(b => b);
+
+    // Methode ersetzen, aber originale Implementierung weiter verwenden
+    spyOn(service, 'rateUp').and.callThrough();
+
+    // Buch
+    const book = { isbn: '123' } as Book; // Type Assertion – bitte nicht produktiv verwenden
+
+    // Act
+    // Methode auf Dashboard aufrufen
+    component.doRateUp(book);
+
+    // Assert
+    // prüfen, ob Serviemethode aufgerufen wurde
+    expect(service.rateUp).toHaveBeenCalled();
+    expect(service.rateUp).toHaveBeenCalledOnceWith(book);
   });
 });
