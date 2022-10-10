fee5d5040b2e5db4cfd9416eef64ab8a4cf456fc
Ferdinand Malcher <ferdinand@malcher.media>
2022-10-10 14:22:07 +0200

    Validierung

--- book-rating/src/app/books/book-create/book-create.component.ts
+++ book-rating/src/app/books/book-create/book-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'br-book-create',
@@ -10,19 +10,36 @@ export class BookCreateComponent implements OnInit {
 
   bookForm = new FormGroup({
     isbn: new FormControl('', {
-      nonNullable: true
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(13),
+        Validators.pattern(/^[0-9]*$/)
+      ]
     }),
     title: new FormControl('', {
-      nonNullable: true
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.maxLength(128)
+      ]
     }),
     description: new FormControl('', {
       nonNullable: true
     }),
     price: new FormControl(0, {
-      nonNullable: true
+      nonNullable: true,
+      validators: [
+        Validators.min(0)
+      ]
     }),
     rating: new FormControl(1, {
-      nonNullable: true
+      nonNullable: true,
+      validators: [
+        Validators.min(1),
+        Validators.max(5)
+      ]
     })
   });
 
