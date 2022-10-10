c65df629c032df8b33b9405d9583a49f6548182c
Ferdinand Malcher <ferdinand@malcher.media>
2022-10-10 12:42:48 +0200

    Vorbereitung Formulae und Suche

--- book-rating/src/app/books/books.module.ts
+++ book-rating/src/app/books/books.module.ts
@@ -5,13 +5,17 @@ import { BooksRoutingModule } from './books-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookComponent } from './book/book.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
+import { BookCreateComponent } from './book-create/book-create.component';
+import { BookSearchComponent } from './book-search/book-search.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
     BookComponent,
-    BookDetailsComponent
+    BookDetailsComponent,
+    BookCreateComponent,
+    BookSearchComponent
   ],
   imports: [
     CommonModule,
