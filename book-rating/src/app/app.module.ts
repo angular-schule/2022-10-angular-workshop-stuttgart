9e5107f7f9f1515be1217ccb4a1c6ef636835da6
Ferdinand Malcher <ferdinand@malcher.media>
2022-10-11 11:42:39 +0200

    Locale setzen

--- book-rating/src/app/app.module.ts
+++ book-rating/src/app/app.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import localeDe from '@angular/common/locales/de-AT';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { registerLocaleData } from '@angular/common';
 // import { BooksModule } from './books/books.module';
+
+registerLocaleData(localeDe);
 
 @NgModule({
   declarations: [
@@ -16,7 +20,9 @@ import { AppComponent } from './app.component';
     // BooksModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'de-AT' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
