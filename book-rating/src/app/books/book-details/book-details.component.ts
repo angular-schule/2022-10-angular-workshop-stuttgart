51c073a9d21a974f4785405934f4310f0646afa3
Ferdinand Malcher <ferdinand@malcher.media>
2022-10-12 15:37:15 +0200

    Detailseite mit AsyncPipe

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -11,23 +11,17 @@ import { BookStoreService } from '../shared/book-store.service';
 })
 export class BookDetailsComponent {
 
-  book?: Book;
+  book$ = this.route.paramMap.pipe(
+    map(params => params.get('isbn')!),
+    switchMap(isbn => this.bs.getSingle(isbn))
+  );
 
   constructor(private route: ActivatedRoute, private bs: BookStoreService) {
     // PULL / Synchroner Weg
     // const isbn = this.route.snapshot.paramMap.get('isbn'); // books/:isbn
 
     // PUSH / Asynchroner Weg
-
-    this.route.paramMap.pipe(
-      map(params => params.get('isbn')!),
-      switchMap(isbn => this.bs.getSingle(isbn))
-    ).subscribe(book => {
-      this.book = book;
-    });
-
-
-
+    // siehe oben
   }
 
 }
